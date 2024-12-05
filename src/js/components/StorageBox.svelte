7b92eb6f9b85da8e6859dc107737fd3c0f0ee136
jm-connell <con18013@byui.edu>
2024-12-04 17:54:40 -0700

    created storagebox

---
+++ src/js/components/StorageBox.svelte
@@ -0,0 +1,13 @@
+<script>
+    {/* This will get data from DB about each pokemon */}
+    {/* Maybe we want a utils.js to have functions like getPokemonDetails */}
+  </script>;
+  
+  <div class="storage-box">
+    <img src="path/to/image.jpg" alt="Pokemon Preview" />
+    <h2>Sample Name</h2>
+    <p>Status: Awake</p>
+    <p>Hunger: 24%</p>
+    <p>Happiness: 87%</p>
+  </div>;
+  
