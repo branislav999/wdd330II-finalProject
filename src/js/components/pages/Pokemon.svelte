b93129163a2bca69a0be1765e6666f37827adddd
jm-connell <jonconnell2@gmail.com>
2024-12-05 13:29:17 -0700

    added new page components

---
+++ src/js/components/pages/Pokemon.svelte
@@ -0,0 +1,2 @@
+<h1>Pokemon Name</h1>
+<p>This page will show the pokemon that you clicked on from the Storage page. It will show the pokemon, its status, and actions you can take.</p>
---
+++ src/js/components/pages/Shop.svelte
@@ -0,0 +1,28 @@
+<script>
+    // Define the items and their prices
+    const items = [
+      { name: 'Pokémon Berry', price: '$5' },
+      { name: 'Pokémon Egg', price: '$20' },
+      { name: 'Soap', price: '$2' }
+    ];
+  </script>
+  
+  <main>
+    <h1>Shop</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each items as item}
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </main>
