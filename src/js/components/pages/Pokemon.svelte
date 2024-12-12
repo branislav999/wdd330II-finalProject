6583da5d2bd2b21ffc3a3a6e4f8c8a83945aad5d
jm-connell <con18013@byui.edu>
2024-12-11 19:18:35 -0700

    remove placeholder text

--- src/js/components/pages/Pokemon.svelte
+++ src/js/components/pages/Pokemon.svelte
@@ -5,7 +5,5 @@
     
 </script>
 
-<p>This page will show the pokemon that you clicked on from the Storage page. It will show the pokemon, its status, and actions you can take.</p>
-
 <PokemonDisplay />
 <Stats />
