4cbd7247138adaa1d3e4e2d1346efa31c38516be
jm-connell <con18013@byui.edu>
2024-12-17 17:37:19 -0700

    run formatting

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,16 @@
 export async function fetchPokemon(name) {
-  const pokemonName = name.toLowerCase();  
-  console.log(`link that fetches it https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+  const pokemonName = name.toLowerCase();
+  console.log(
+    `link that fetches it https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
   if (!response.ok) {
     throw new Error('Pokemon not found!');
   }
   return await response.json();
 }
-
 
 export async function getSprite(name) {
   const data = await fetchPokemon(name);
--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://caugfpweiibttpqkbaoj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhdWdmcHdlaWlidHRwcWtiYW9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3Mzc1MjEsImV4cCI6MjA0OTMxMzUyMX0.Wziv1efmwl5iuOfTXUU71BgcF5D9aKngC9rWNtEufFE';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhdWdmcHdlaWlidHRwcWtiYW9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3Mzc1MjEsImV4cCI6MjA0OTMxMzUyMX0.Wziv1efmwl5iuOfTXUU71BgcF5D9aKngC9rWNtEufFE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
